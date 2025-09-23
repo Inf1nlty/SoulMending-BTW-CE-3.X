f9197e93eb11c0f6784ff31fa123056e035d89de
Inf1nity <1449896523@qq.com>
2025-09-24 03:35:46 +0800

    Add wolf possess

--- src/main/java/com/inf1nlty/soultotem/mixin/entity/EntityCreatureMixin.java
+++ src/main/java/com/inf1nlty/soultotem/mixin/entity/EntityCreatureMixin.java
@@ -6,54 +6,48 @@ import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import java.util.List;
 
 @Mixin(EntityCreature.class)
-public abstract class EntityCreatureMixin implements ISoulPossessable {
+public abstract class EntityCreatureMixin {
 
-    @Override
-    public void soulMending$onSoulPossession() {
-    }
+    @Inject(method = "attemptToPossessNearbyCreature", at = @At("TAIL"))
+    public void onAttemptToPossessNearbyCreature(double dRange, boolean bPersistentSpirit, PossessionSource<?> possessionSource, CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue()) return;
 
-    @Inject(method = "initiatePossession", at = @At("TAIL"))
-    public void onInitiatePossession(PossessionSource<?> possessionSource, CallbackInfo ci) {
-        this.soulMending$onSoulPossession();
-    }
+        EntityCreature self = (EntityCreature) (Object) this;
+        World world = self.worldObj;
 
-    @Inject(method = "onDeath", at = @At("RETURN"))
-    public void onDeathInject(DamageSource source, CallbackInfo ci) {
-        EntityCreature self = (EntityCreature)(Object)this;
-
-        if (!self.worldObj.isRemote && self.isPossessed()) {
-            double range = 16D;
-            double x = self.posX, y = self.posY, z = self.posZ;
-            World world = self.worldObj;
-            AxisAlignedBB box = AxisAlignedBB.getAABBPool().getAABB(
-                    x - range, y - range, z - range,
-                    x + range, y + range, z + range
+        if (!world.isRemote) {
+            AxisAlignedBB aabb = AxisAlignedBB.getAABBPool().getAABB(
+                    self.posX - dRange, self.posY - dRange, self.posZ - dRange,
+                    self.posX + dRange, self.posY + dRange, self.posZ + dRange
             );
 
             // Entity
             @SuppressWarnings("unchecked")
-            List<Entity> entityList = world.getEntitiesWithinAABB(Entity.class, box);
+            List<Entity> entityList = world.getEntitiesWithinAABB(Entity.class, aabb);
             for (Entity entity : entityList) {
                 if (entity != self && entity.isEntityAlive()
                         && !(entity instanceof EntityCreature)
-                        && entity instanceof ISoulPossessable possessable) {
-                    possessable.soulMending$onSoulPossession();
+                        && entity instanceof ISoulPossessable) {
+                    ((ISoulPossessable) entity).soulMending$onSoulPossession();
+                    return;
                 }
             }
 
             // TileEntity
             @SuppressWarnings("unchecked")
-            List<TileEntity> tileList = (List<TileEntity>)world.loadedTileEntityList;
-            for (TileEntity tile : tileList) {
-                if (tile.xCoord >= box.minX && tile.xCoord <= box.maxX &&
-                        tile.yCoord >= box.minY && tile.yCoord <= box.maxY &&
-                        tile.zCoord >= box.minZ && tile.zCoord <= box.maxZ &&
-                        tile instanceof ISoulPossessable possessable) {
-                    possessable.soulMending$onSoulPossession();
+            List<TileEntity> tileEntityList = (List<TileEntity>) world.loadedTileEntityList;
+            for (TileEntity tileEntity : tileEntityList) {
+                if (tileEntity.xCoord >= aabb.minX && tileEntity.xCoord <= aabb.maxX &&
+                        tileEntity.yCoord >= aabb.minY && tileEntity.yCoord <= aabb.maxY &&
+                        tileEntity.zCoord >= aabb.minZ && tileEntity.zCoord <= aabb.maxZ &&
+                        tileEntity instanceof ISoulPossessable) {
+                    ((ISoulPossessable) tileEntity).soulMending$onSoulPossession();
+                    return;
                 }
             }
         }
